e3fb93a7360e69c81a0ab831539dce2e6cae152e
Philipp <skyn3t.syst3m@gmail.com>
2019-03-04 20:18:17 +0100

    Update to Version 1.4
    new installer

--- assets/tools/install_monitor.sh
+++ assets/tools/install_monitor.sh
@@ -2,7 +2,7 @@
 # Created by didiatworkz
 # Screenly OSE Monitor 
 #
-# October 2018
+# March 2019
 
 header() {
 clear
@@ -17,94 +17,65 @@ cat << "EOF"
 
 EOF
 echo
-echo "Screenly OSE Monitor"
+echo "         Screenly OSE Monitoring"
 echo
 echo
 }
 
 header
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
+echo
+echo
+read -p "Do you want to install the Screenly OSE Monitoring? (y/N)" -n 1 -r -s INSTALL_BEGIN
 
-echo && read -p "Do you want to install Screenly OSE Monitor? (y/N)" -n 1 -r -s START && echo
-if [ "$START" != 'y' ]; then
-  exit
-fi
-
-# Check if old version exists
-if [ -e /var/www/html/assets/tools/version.txt ]; then
-    UPDATE=true
+if [ "$INSTALL_BEGIN" != 'y' ]
+then
+    echo
+    exit
 fi
 
 header
+echo
+
 # Check if screenly exists
-if [ -e /home/pi/screenly/server.py ]; then
-    SCREENLY=true
-	echo && read -p "Do you want to install the Monitor Extension for Screenly? (y/N)" -n 1 -r -s UPGRADE && echo
-	if [ "$UPGRADE" == 'y' ]; then
-	  MONITOR_EXTENSION=true
-	else
-	  MONITOR_EXTENSION=false
-	fi
+echo Check if Screenly installed...
+echo
+if [ ! -e /home/pi/screenly/server.py ]
+then
+    echo -e "[ \e[32mNO\e[39m ] Screenly installed"
+	echo
+	echo Installation aborted because no Screenly was found!
+	echo Please check if the file /home/pi/screenly/server.py exists!
+	exit
+
+else
+    echo -e "[ \e[93mYES\e[39m ] Screenly installed"
 fi
-
-# Install packeges
-if [ "$UPDATE" != true ]; then
-	sudo apt update
-    if [ "$SCREENLY" != true ]; then
-		apt install nginx -y
-	fi
-	apt install php-fpm php7.0-sqlite php7.0-curl git -y
-fi
-
-# Clone git repository
-git clone https://github.com/didiatworkz/screenly-ose-monitor.git /tmp/monitor
-
-# Install monitor extension
-if [ "$MONITOR_EXTENSION" = true ]; then
-    cp /tmp/monitor/assets/tools/extension.sh /tmp/extension.sh
-	sed -i 's/reboot now//g' /tmp/extension.sh
-	chmod +x /tmp/extension.sh
-	/tmp/extension.sh "installer"
-fi
-
+sleep 2
 header
-# Copy files and set rights
-mkdir -p /var/www/html
-cp -rf /tmp/monitor/* /var/www/html/
-chown www-data:www-data /var/www/html
-chown www-data:www-data /var/www/html/*
+echo -e "\e[94mStart installation...\e[39m"
+sleep 5
 
-# Create nginx config
-cat >/etc/nginx/sites-enabled/monitor.conf <<EOF
-server {
-
-        #Nginx should listen on port 80 for requests to yoursite.com
-        listen 9000;
-        server_name _;
-
-
-        root /var/www/html/;
-        index index.php;
-
-        location ~ \.php$ {
-                include /etc/nginx/fastcgi.conf;
-                fastcgi_pass unix:/run/php/php7.0-fpm.sock;
-        }
-}
-EOF
-
-# Restart nginx
-systemctl restart nginx
+sudo mkdir -p /var/www/html/monitor
+ansible localhost -m git -a "repo=${1:-https://github.com/didiatworkz/screenly-ose-monitor.git} dest=/var/www/html/monitor version=master"
+cd  /var/www/html/monitor/assets/tools/ansible
+ansible-playbook site.yml
+cd /var/www/html/monitor/ && git rev-parse HEAD > ~/.monitor/latest_monitor
 IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-
+sleep 2
 header
-echo "Installation finished!"
+echo -e "\e[94mInstallation finished!"
 echo
 echo
-echo "You can now reach the Screenly OSE Monitor at the address: http://$IP:9000"
+echo -e "You can now reach the Screenly OSE Monitor at the address: \n\e[93mhttp://$IP:9000\e[39m"
+echo
+echo -e "\e[94mUsername: \e[93mdemo\e[39m"
+echo -e "\e[94mPassword: \e[93mdemo\e[39m"
+echo
+echo
+echo
 exit
+exit
+
+
 
 
