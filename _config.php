6a4098001ced137d015c9cc1ba373ca0198889bb
Philipp Dyhring <skyn3t.syst3m@gmail.com>
2019-10-01 19:11:16 +0200

    Fix update menu item

--- _config.php
+++ _config.php
@@ -167,8 +167,14 @@
 	if(@file_exists('update.txt')) {
 		$update = '
 					<li class="nav-item">
-						<a href="https://github.com/didiatworkz/screenly-ose-monitor" target="_blank" class="nav-link" title="Update available!"><i class="tim-icons icon-cloud-download-93 blink"></i></a>
-					</li>';
+						<a https://github.com/didiatworkz/screenly-ose-monitor" target="_blank" class="nav-link" data-tooltip="tooltip" data-placement="bottom" title="Update available!">
+							<i class="tim-icons icon-cloud-download-93 blink"></i>
+							<p class="d-lg-none">
+								Update available!
+							</p>
+						</a>
+					</li>
+		';
 
 	}
 	else $update = '';
