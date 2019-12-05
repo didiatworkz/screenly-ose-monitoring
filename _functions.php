e453b2df72212d45e1c5e8928cd3ef42cbe9df47
Philipp Dyhring <skyn3t.syst3m@gmail.com>
2019-12-05 22:00:23 +0100

    Update _functions.php

--- _functions.php
+++ _functions.php
@@ -20,7 +20,7 @@
 	ini_set('display_errors',1);
 	error_reporting(E_ALL|E_STRICT);
 	date_default_timezone_set("Europe/Berlin");
-	$apiVersion		  = 'v1.2';
+	$apiVersion	= 'v1.2';
 
 
 /* _______________________________ */
