f3c1716c8ef0b3660ff36dc44cc3355c35427bd6
Philipp Dyhring <skyn3t.syst3m@gmail.com>
2020-05-21 21:06:29 +0200

    change url

--- assets/php/update.php
+++ assets/php/update.php
@@ -24,7 +24,7 @@ if($updatecheck < time() && (date("d", $updatecheck) != date("d"))){
 if(@file_exists('update.txt')) {
   $update = '
         <li class="nav-item">
-          <a href="https://github.com/didiatworkz/screenly-ose-monitor" target="_blank" class="nav-link" data-tooltip="tooltip" data-placement="bottom" title="Update available!">
+          <a href="https://github.com/didiatworkz/screenly-ose-monitor#update" target="_blank" class="nav-link" data-tooltip="tooltip" data-placement="bottom" title="Update available!">
             <i class="tim-icons icon-cloud-download-93 blink"></i>
             <p class="d-lg-none">
               Update available!
