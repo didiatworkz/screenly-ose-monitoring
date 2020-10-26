76db466135df7b5667e88bb5ed2fefa45fe6ac98
Philipp Dyhring <skyn3t.syst3m@gmail.com>
2020-10-26 23:08:19 +0100

    fix href

--- assets/php/menu.php
+++ assets/php/menu.php
@@ -70,7 +70,7 @@ echo'
           </div>
         </a>
         <div class="dropdown-menu dropdown-menu-right">
-          <a class="dropdown-item" href="#">
+          <a class="dropdown-item" href="index.php?action=logout">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon dropdown-item-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path><path d="M7 12h14l-3 -3m0 6l3 -3"></path></svg>
             Logout
           </a>
