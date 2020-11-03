08b00ce4d0493105d3c06b36d37f5c585bdb791f
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2020-11-03 22:16:40 +0100

    fix missingmenu in handheld mode

--- assets/php/menu.php
+++ assets/php/menu.php
@@ -54,6 +54,11 @@ echo'
     </a>
     <div class="navbar-nav flex-row order-md-last">
       <div class="nav-item dropdown d-none d-md-flex mr-3">
+        <a href="#" data-toggle="modal" data-target="#newPlayer" class="nav-link px-0" tabindex="-1">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>
+        </a>
+      </div>
+      <div class="nav-item dropdown d-none d-md-flex mr-3">
         <a href="'.$_SERVER['REQUEST_URI'].'" class="nav-link px-0" tabindex="-1">
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M4.05 11a8 8 0 1 1 .5 4m-.5 5v-5h5"></path></svg>
         </a>
@@ -109,6 +114,28 @@ echo'
               </span>
               <span class="nav-link-title">
                 '.Translation::of('addon').'
+              </span>
+            </a>
+          </li>
+
+          <li class="nav-item d-block d-sm-none">
+            <a class="nav-link" href="index.php?site=settings">
+              <span class="nav-link-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+              </span>
+              <span class="nav-link-title">
+                '.Translation::of('settings').'
+              </span>
+            </a>
+          </li>
+
+          <li class="nav-item d-block d-sm-none">
+            <a class="nav-link" href="index.php?action=logout">
+              <span class="nav-link-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path><path d="M7 12h14l-3 -3m0 6l3 -3"></path></svg>
+              </span>
+              <span class="nav-link-title">
+                '.Translation::of('logout').'
               </span>
             </a>
           </li>
