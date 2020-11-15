78a56e7ccbe0e259d6df5d9572b8e09447a62a69
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2020-11-15 21:49:43 +0100

    Update settings.php

--- assets/php/settings.php
+++ assets/php/settings.php
@@ -237,7 +237,7 @@ else if(isset($_GET['view']) && $_GET['view'] == 'system'){
 }
 else if(isset($_GET['view']) && $_GET['view'] == 'publicaccess'){
 
-  $tokenLink = 'http://'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'].'/index.php?public=1&key='.$set['token'];
+  $tokenLink = 'http://'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'].'/_public.php?key='.$set['token'];
 
   echo '
   <div class="container-xl">
@@ -257,24 +257,152 @@ else if(isset($_GET['view']) && $_GET['view'] == 'publicaccess'){
       </div>
     </div>
 
-    <div class="card card-lg">
-      <form id="settingsForm" action="'.$_SERVER['REQUEST_URI'].'" method="POST" data-toggle="validator">
-        <div class="card-body">
-          <div class="form-group">
-            <label for="InputSetName">'.Translation::of('public_access_link').'</label>
-            <input type="text" class="form-control" id="InputSetToken" onClick="this.select();" value="'.$tokenLink.'" />
+    <div class="row justify-content-center">
+      <div class="col-lg-3 order-lg-1 mb-4">
+        <div class="sticky-top">
+          <div class="card">
+            <div class="card-body text-center">
+              <button class="btn btn-outline-info btn-block mb-2" id="open_token">Open in new window</button>
+              <button class="btn btn-outline-secondary btn-block mb-2" id="add_dark">Activate Darkmode</button>
+            </div>
           </div>
         </div>
-        <div class="card-footer d-flex align-items-center">
-          <a href="index.php?site=settings" class="btn btn-link mr-auto">'.Translation::of('cancel').'</a>
-          <a href="'.$tokenLink.'" target="_blank" class="btn btn-secondary mr-2">Open Link</a>
-          <a href="index.php?site=settings&view=publicaccess&generateToken=yes" class="btn btn-primary">'.Translation::of('generate_token').'</a>
+      </div>
+      <div class="col-lg-9">
+        <div class="card card-lg">
+          <form id="settingsForm" action="'.$_SERVER['REQUEST_URI'].'" method="POST" data-toggle="validator">
+            <div class="card-body">
+              <div class="form-group">
+                <label for="InputSetName">'.Translation::of('public_access_link').'</label>
+                <input type="text" class="form-control" id="InputSetToken" onClick="this.select();" value="'.$tokenLink.'" />
+              </div>
+            </div>
+            <div class="card-footer d-flex align-items-center">
+              <a href="index.php?site=settings" class="btn btn-link mr-auto">'.Translation::of('cancel').'</a>
+              <a href="index.php?site=settings&view=publicaccess&generateToken=yes" class="btn btn-primary">'.Translation::of('generate_token').'</a>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   </div>
 
   ';
+}
+else if(isset($_GET['view']) && $_GET['view'] == 'log'){
+  if(isset($_GET['action']) && $_GET['action'] == 'reset'){
+    systemLog('Settings', 'Admin Log: Start Log reset', $loginUserID, 0);
+    $db->exec("DELETE FROM log");
+    //sysinfo('success', Translation::of('msg.player_delete_successfully'));
+    redirect($backLink, 0);
+  }
+  else {
+    $logSQL = $db->query("SELECT * FROM `log` ORDER BY logTime");
+    echo '
+    <div class="container-xl">
+      <div class="page-header">
+        <div class="row align-items-center">
+          <div class="col-auto">
+            <h2 class="page-title">
+              Admin Log
+            </h2>
+            <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+              <li class="breadcrumb-item"><a href="index.php?site=settings">Settings</a></li>
+              <li class="breadcrumb-item active" aria-current="page"><a href="index.php?site=settings&view=log">Admin Log</a></li>
+            </ol>
+          </div>
+          <div class="col-auto ml-auto">
+            <a href="#" data-toggle="modal" data-target="#confirmDeleteLog" data-href="index.php?site=settings&view=log&action=reset" class="btn btn-danger"  title="delete">
+              Reset Log
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Log Data</h3>
+        </div>
+        <div class="card-body border-bottom py-3">
+          <div class="d-flex">
+            <div class="text-muted">
+              Show
+              <div class="mx-2 d-inline-block">
+                <select class="form-select form-select-sm" id="logLength_change">
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+                <option value="-1">All</option>
+                </select>
+              </div>
+              entries
+            </div>
+            <div class="ml-auto text-muted">
+              Search:
+              <div class="ml-2 d-inline-block">
+                <input type="text" class="form-control form-control-sm" id="logSearch">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="table-responsive">
+
+      <table class="table vertical-center" id="log">
+        <thead class="text-primary">
+          <tr>
+            <th>Log Time</th>
+            <th>User</th>
+            <th>Modul</th>
+            <th>Info</th>
+          </tr>
+        </thead>
+        <tbody>
+            ';
+        while($log = $logSQL->fetchArray(SQLITE3_ASSOC)){
+          if($log['userID'] == 0) $user = 'SYSTEM';
+          else $user = getUserName($log['userID']);
+          echo '
+            <tr>
+              <td>'.date('Y-m-d H:i:s', $log['logTime']).'</td>
+              <td>'.$user.'</td>
+              <td><span class="badge bg-secondary p-1">  '.$log['moduleName'].'  </span></td>
+              <td>'.$log['info'].'</td>
+            </tr>
+                ';
+        }
+    echo '
+        </tbody>
+      </table>
+    </div>
+    <div class="card-footer d-flex align-items-center">
+      <p class="m-0 text-muted" id="dataTables_info"></p>
+      <span class="pagination m-0 ml-auto" id="dataTables_paginate"></span>
+    </div>
+    ';
+    echo '
+
+  </div>
+</div>
+<!-- confirmDeleteLog -->
+<div class="modal modal-blur fade" id="confirmDeleteLog" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteLog" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">'.Translation::of('attention').'!</h5>
+      </div>
+      <div class="modal-body">
+        Do you really want to reset the log files?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-link mr-auto" data-dismiss="modal">'.Translation::of('cancel').'</button>
+        <a class="btn btn-danger btn-ok">'.Translation::of('delete').'</a>
+      </div>
+    </div>
+  </div>
+</div>
+    ';
+  }
 }
 else {
   echo '
@@ -291,92 +419,100 @@ else {
       </div>
     </div>
     <div class="row">
-    '.$update_card.'
+    '.$update_info.'
+    </div>
+    <div class="row row-deck">
       <div class="col">
         <div class="card">
-          <div class="card-body text-center">
-            <div class="mb-3">
-              <span class="avatar avatar-xl"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><circle cx="12" cy="7" r="4"></circle><path d="M5.5 21v-2a4 4 0 0 1 4 -4h5a4 4 0 0 1 4 4v2"></path></svg></span>
+          <a href="index.php?site=settings&view=profile">
+            <div class="card-body text-center">
+              <div class="mb-3">
+                <span class="avatar avatar-xl"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><circle cx="12" cy="7" r="4"></circle><path d="M5.5 21v-2a4 4 0 0 1 4 -4h5a4 4 0 0 1 4 4v2"></path></svg></span>
+              </div>
+              <div class="card-title mb-1">Profile Settings</div>
+              <div class="text-muted">Name, Login, Design</div>
             </div>
-            <div class="card-title mb-1">Profile Settings</div>
-            <div class="text-muted">Name, Login, Design</div>
-          </div>
-          <a href="index.php?site=settings&view=profile" class="card-btn">Show</a>
+          </a>
         </div>
       </div>';
       if($loginGroupID == 1){
       echo'
       <div class="col-md-6">
         <div class="card">
-          <div class="card-body text-center">
-            <div class="mb-3">
-              <span class="avatar avatar-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M7 10h3v-3l-3.5 -3.5a6 6 0 0 1 8 8l6 6a2 2 0 0 1 -3 3l-6-6a6 6 0 0 1 -8 -8l3.5 3.5"></path></svg>
-              </span>
+          <a href="index.php?site=settings&view=system">
+            <div class="card-body text-center">
+              <div class="mb-3">
+                <span class="avatar avatar-xl">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M7 10h3v-3l-3.5 -3.5a6 6 0 0 1 8 8l6 6a2 2 0 0 1 -3 3l-6-6a6 6 0 0 1 -8 -8l3.5 3.5"></path></svg>
+                </span>
+              </div>
+              <div class="card-title mb-1">System Settings</div>
+              <div class="text-muted">API Settings, Default Settings, Design</div>
             </div>
-            <div class="card-title mb-1">System Settings</div>
-            <div class="text-muted">API Settings, Default Settings, Design</div>
           </div>
-          <a href="index.php?site=settings&view=system" class="card-btn">Show</a>
-        </div>
+        </a>
       </div>
     </div>
-    <div class="row">
+    <div class="row row-deck">
       <div class="col-md-6 col-xl-3">
         <div class="card">
-          <div class="card-body text-center">
-            <div class="mb-3">
-              <span class="avatar avatar-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><circle cx="12" cy="12" r="9"></circle><line x1="3.6" y1="9" x2="20.4" y2="9"></line><line x1="3.6" y1="15" x2="20.4" y2="15"></line><path d="M11.5 3a17 17 0 0 0 0 18"></path><path d="M12.5 3a17 17 0 0 1 0 18"></path></svg>
-              </span>
+          <a href="index.php?site=settings&view=publicaccess">
+            <div class="card-body text-center">
+              <div class="mb-3">
+                <span class="avatar avatar-xl">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><circle cx="12" cy="12" r="9"></circle><line x1="3.6" y1="9" x2="20.4" y2="9"></line><line x1="3.6" y1="15" x2="20.4" y2="15"></line><path d="M11.5 3a17 17 0 0 0 0 18"></path><path d="M12.5 3a17 17 0 0 1 0 18"></path></svg>
+                </span>
+              </div>
+              <div class="card-title mb-1">Public Access Settings</div>
+              <div class="text-muted">Design, Link</div>
             </div>
-            <div class="card-title mb-1">Public Access Settings</div>
-            <div class="text-muted">Design, Link</div>
           </div>
-          <a href="index.php?site=settings&view=publicaccess" class="card-btn">Show</a>
-        </div>
+        </a>
       </div>
       <div class="col-md-6 col-xl-3">
         <div class="card">
-          <div class="card-body text-center">
-            <div class="mb-3">
-              <span class="avatar avatar-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11"></path><line x1="8" y1="8" x2="12" y2="8"></line><line x1="8" y1="12" x2="12" y2="12"></line><line x1="8" y1="16" x2="12" y2="16"></line></svg>
-              </span>
+          <a href="index.php?site=settings&view=log">
+            <div class="card-body text-center">
+              <div class="mb-3">
+                <span class="avatar avatar-xl">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11"></path><line x1="8" y1="8" x2="12" y2="8"></line><line x1="8" y1="12" x2="12" y2="12"></line><line x1="8" y1="16" x2="12" y2="16"></line></svg>
+                </span>
+              </div>
+              <div class="card-title mb-1">Admin Logs</div>
+              <div class="text-muted">Protocols, Activities</div>
             </div>
-            <div class="card-title mb-1">Admin Logs</div>
-            <div class="text-muted">Protocols, Activities</div>
           </div>
-          <a href="#" class="card-btn">Show</a>
-        </div>
+        </a>
       </div>
       <div class="col-md-6 col-xl-3">
         <div class="card">
-          <div class="card-body text-center">
-            <div class="mb-3">
-              <span class="avatar avatar-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><line x1="9.5" y1="11" x2="9.51" y2="11"></line><line x1="14.5" y1="11" x2="14.51" y2="11"></line><path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path><path d="M7 5h1v-2h8v2h1a3 3 0 0 1 3 3v9a3 3 0 0 1 -3 3v1h-10v-1a3 3 0 0 1 -3 -3v-9a3 3 0 0 1 3 -3"></path></svg>
-              </span>
+          <a href="index.php?site=usermanagement">
+            <div class="card-body text-center">
+              <div class="mb-3">
+                <span class="avatar avatar-xl">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><line x1="9.5" y1="11" x2="9.51" y2="11"></line><line x1="14.5" y1="11" x2="14.51" y2="11"></line><path d="M9.5 15a3.5 3.5 0 0 0 5 0"></path><path d="M7 5h1v-2h8v2h1a3 3 0 0 1 3 3v9a3 3 0 0 1 -3 3v1h-10v-1a3 3 0 0 1 -3 -3v-9a3 3 0 0 1 3 -3"></path></svg>
+                </span>
+              </div>
+              <div class="card-title mb-1">User Settings</div>
+              <div class="text-muted">Create, Edit, Remove</div>
             </div>
-            <div class="card-title mb-1">User Settings</div>
-            <div class="text-muted">Create, Edit, Remove</div>
           </div>
-          <a href="index.php?site=usermanagement" class="card-btn">Show</a>
-        </div>
+        </a>
       </div>
       <div class="col-md-6 col-xl-3">
         <div class="card">
-          <div class="card-body text-center">
-            <div class="mb-3">
-              <span class="avatar avatar-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><rect x="5" y="11" width="14" height="10" rx="2"></rect><circle cx="12" cy="16" r="1"></circle><path d="M8 11v-5a4 4 0 0 1 8 0"></path></svg>
-              </span>
+          <a href="#">
+            <div class="card-body text-center">
+              <div class="mb-3">
+                <span class="avatar avatar-xl">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><rect x="5" y="11" width="14" height="10" rx="2"></rect><circle cx="12" cy="16" r="1"></circle><path d="M8 11v-5a4 4 0 0 1 8 0"></path></svg>
+                </span>
+              </div>
+              <div class="card-title mb-1">Group Settings</div>
+              <div class="text-muted">Create Groups, Add Members</div>
             </div>
-            <div class="card-title mb-1">User Rights</div>
-            <div class="text-muted">Create Groups, Add Members</div>
           </div>
-          <a href="#" class="card-btn">Show</a>
-        </div>
+        </a>
       </div>
     </div>';
   }
