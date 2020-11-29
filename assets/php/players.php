996451cb7eea12a8ea9107cb151787b07d5c7ed0
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2020-11-29 18:50:43 +0100

    change function

--- assets/php/players.php
+++ assets/php/players.php
@@ -34,7 +34,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'view'){
     $sqlCount 	= $db->query("SELECT COUNT(*) FROM player WHERE playerID='".$playerID."'");
     $count 		  = $sqlCount->fetchArray(SQLITE3_ASSOC);
     $count      = $count['COUNT(*)'];
-    if($count > 0){
+    if(getPlayerCount() > 0){
       $playerSQL 	= $db->query("SELECT * FROM player WHERE playerID='".$playerID."'");
       $player 		= $playerSQL->fetchArray(SQLITE3_ASSOC);
       $monitor 		= 0;
@@ -65,7 +65,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'view'){
         </svg>
       </a>';
 
-      if(hasPlayerDeleteRight($loginUserID)) $player_delete_btn = '<a href="#" data-toggle="modal" data-target="#confirmMessage" data-status="danger" data-href="'.$_moduleLink.'&action=delete&playerID='.$player['playerID'].'">
+      if(hasPlayerDeleteRight($loginUserID)) $player_delete_btn = '<a href="#" data-toggle="modal" data-target="#confirmMessage" data-status="danger" data-text="'.Translation::of('msg.delete_really_entry').'" data-href="'.$_moduleLink.'&action=delete&playerID='.$player['playerID'].'">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><line x1="4" y1="7" x2="20" y2="7"></line><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path></svg>
       </a>';
 
