c9904dc93e52a5f7651e42bc3bde7a2d811ce9d5
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2020-11-29 18:49:14 +0100

    Update functions.php

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -70,3 +70,5 @@ function systemLog($module, $info, $who = 0, $show = 0, $relevant = 0){
   $when = time();
   $db->exec("INSERT INTO `log` (userID, logTime, moduleName, info, show, relevant) values('".$who."', '".$when."', '".$module."', '".$info."', '".$show."', '".$relevant."')");
 }
+
+if(isset($_GET['somo_link'])) redirect('https://github.com/didiatworkz/screenly-ose-monitor', 0);
