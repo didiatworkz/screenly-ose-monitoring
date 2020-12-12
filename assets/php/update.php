3e6e64861f522f6789d4c26efaefc996df518630
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2020-12-12 23:15:32 +0100

    change color

--- assets/php/update.php
+++ assets/php/update.php
@@ -36,7 +36,7 @@ if($updatecheck < time() && (date("d", $updatecheck) != date("d"))){
 if(@file_exists('update.txt') && isAdmin($loginUserID)) {
   $update_badge = '<span class="badge bg-red blink"></span>';
   $update_info = '
-  <div class="alert alert-info text-center" role="alert">
+  <div class="alert alert-warning text-center" role="alert">
   <svg xmlns="http://www.w3.org/2000/svg" class="icon mr-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="9" /><line x1="12" y1="8" x2="12.01" y2="8" /><polyline points="11 12 12 12 12 16 13 16" /></svg>
   SOMO information — <a href="https://github.com/didiatworkz/screenly-ose-monitor/releases" target="_blank" class="blink">'.Translation::of('update_available').'</a>
 </div>
