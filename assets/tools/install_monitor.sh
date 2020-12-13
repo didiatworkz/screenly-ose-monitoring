245161dd23d4b368cf29d852ba1cbc24cc52f682
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2020-12-13 13:55:16 +0100

    add details

--- assets/tools/install_monitor.sh
+++ assets/tools/install_monitor.sh
@@ -41,20 +41,25 @@ then
 fi
 
 echo
-
-# Check if screenly exists
-echo Check if Screenly installed...
+echo -e "[ \e[33mSOMO\e[39m ] Check if Screenly installed..."
 echo
 if [ ! -e /home/pi/screenly/server.py ]
 then
   echo -e "[ \e[32mNO\e[39m ] Screenly installed"
-  echo -e "[ \e[93mYES\e[39m ] Standalone Installation"
   echo "----------------------------------------------"
+  echo
+  echo -e "[ \e[33mSOMO\e[39m ] Start Server installation preperation"
+  echo -e "[ \e[33mSOMO\e[39m ] Create /etc/ansible folder"
   sudo mkdir -p /etc/ansible
+  echo -e "[ \e[33mSOMO\e[39m ] Add localhost connection to /etc/ansible/hosts"
   echo -e "[local]\nlocalhost ansible_connection=local" | sudo tee /etc/ansible/hosts > /dev/null
+  echo -e "[ \e[33mSOMO\e[39m ] Update system"
   sudo apt update
+  echo -e "[ \e[33mSOMO\e[39m ] Remove old package"
   sudo apt-get purge -y python-setuptools python-pip python-pyasn1 libffi-dev
+  echo -e "[ \e[33mSOMO\e[39m ] Install new packages"
   sudo apt-get install -y python3-dev git-core libffi-dev libssl-dev
+  echo -e "[ \e[33mSOMO\e[39m ] Install pip3 via python3"
   curl -s https://bootstrap.pypa.io/get-pip.py | sudo python3
   sudo pip3 install ansible=="$_ANSIBLE_VERSION"
   _SERVERMODE="listen 80 default_server;"
@@ -68,14 +73,25 @@ fi
 sleep 2
 echo
 echo
-echo -e "\e[94mStart installation...\e[39m"
+echo -e "[ \e[33mSOMO\e[39m ] Start installation..."
 sleep 5
+if [ ! -e /var/www/html/monitor/_functions.php ]
+then
+  _DEMOLOGIN=""
+else
+  _DEMOLOGIN="\e[94mUsername: \e[93mdemo\e[39m \n\e[94mPassword: \e[93mdemo\e[39m"
+fi
+echo -e "[ \e[33mSOMO\e[39m ] Remove old git repository if exists"
 sudo rm -rf /tmp/monitor
+echo -e "[ \e[33mSOMO\e[39m ] Clone repository"
 sudo git clone --branch $_BRANCH https://github.com/didiatworkz/screenly-ose-monitor.git /tmp/monitor
 cd /tmp/monitor/assets/tools/ansible/
+echo -e "[ \e[33mSOMO\e[39m ] Create /var/www/monitor folder"
 sudo mkdir -p /var/www/html
+echo -e "[ \e[33mSOMO\e[39m ] Set installation parameters"
 export SERVER_MODE=$_SERVERMODE
 export MONITOR_BRANCH=$_BRANCH
+echo -e "[ \e[33mSOMO\e[39m ] Start ansible installation"
 sudo -E ansible-playbook site.yml
 sudo systemctl restart nginx
 IP=$(ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}')
@@ -86,13 +102,12 @@ echo
 echo
 echo
 header
-echo -e "\e[94mInstallation finished!"
+echo -e "[ \e[33mSOMO\e[39m ] Installation finished!"
 echo
 echo
-echo -e "You can now start Screenly OSE Monitor with the address: \n\e[93mhttp://$IP$_PORT\e[39m"
+echo -e "You can now start Screenly OSE Monitoring with the address: \n\e[93mhttp://$IP$_PORT\e[39m"
 echo
-echo -e "\e[94mUsername: \e[93mdemo\e[39m"
-echo -e "\e[94mPassword: \e[93mdemo\e[39m"
+echo "$_DEMOLOGIN"
 echo
 echo
 echo
