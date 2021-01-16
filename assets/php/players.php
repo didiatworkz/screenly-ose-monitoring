0bf39f70df281a834043549fa71463e231cfc08f
Philipp Dyhring <skyn3t.syst3m@gmail.com>
2021-01-16 23:31:51 +0100

    fix layout

--- assets/php/players.php
+++ assets/php/players.php
@@ -58,7 +58,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'view'){
       $player_edit_btn = '';
       $player_delete_btn = '';
 
-      if(hasPlayerEditRight($loginUserID)) $player_edit_btn = '<a href="#" data-playerid="'.$player['playerID'].'" class="editPlayerOpen" title="'.Translation::of('edit_player').'">
+      if(hasPlayerEditRight($loginUserID)) $player_edit_btn = '<a href="#" data-playerid="'.$player['playerID'].'" class="editPlayerOpen mr-3" title="'.Translation::of('edit_player').'">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z"></path>
           <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
@@ -66,7 +66,7 @@ if(isset($_GET['action']) && $_GET['action'] == 'view'){
         </svg>
       </a>';
 
-      if(hasPlayerDeleteRight($loginUserID)) $player_delete_btn = '<a href="#" data-toggle="modal" data-target="#confirmMessage" data-status="danger" data-text="'.Translation::of('msg.delete_really_entry').'" data-href="'.$_moduleLink.'&action=delete&playerID='.$player['playerID'].'" title="'.Translation::of('delete_player').'">
+      if(hasPlayerDeleteRight($loginUserID)) $player_delete_btn = '<a href="#" class="text-danger" data-toggle="modal" data-target="#confirmMessage" data-status="danger" data-text="'.Translation::of('msg.delete_really_entry').'" data-href="'.$_moduleLink.'&action=delete&playerID='.$player['playerID'].'" title="'.Translation::of('delete_player').'">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"></path><line x1="4" y1="7" x2="20" y2="7"></line><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path></svg>
       </a>';
 
