b8b41d56dc924aad4ba2ecd07d1ad5d07f05ad59
Philipp Hörauf <skyn3t.syst3m@gmail.com>
2021-10-28 23:03:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,20 +26,26 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
     FILE=/var/www/html/assets/data/database.db
+    DIRECTORY=/var/www/html/assets/data/avatars
     if [ ! -f "$FILE" ]; then
         echo "-- Create Database --"
         cp /var/www/html/dbase.sample.db "$FILE"
+        chown www-data:www-data "$FILE"
         echo "For first login use:"
         echo
         echo "Username: demo"
         echo "Password: demo"
     fi
+    if [ ! -d "$DIRECTORY" ]; then
+        mkdir -p "$DIRECTORY"
+        chown www-data:www-data "$DIRECTORY"
+    fi
 fi
 if [ -x "$(command -v usermod)" ] ; then
     echo "-- Change www-data to user id: $UID --"
-    usermod -u ${UID} www-data
+    usermod -u "${UID}" www-data
     echo "-- Change www-data to group id: $GID --"
-    groupmod -g ${GID} www-data
+    groupmod -g "${GID}" www-data
     echo "-- Activate cronjob --"
     crontab /etc/cron.d/somo
 fi
